d4a6d6db945746e1c49d8665b2ccdd02955a48e3
micpana <michaelmudimbu@gmail.com>
2019-06-21 09:33:42 +0200

    testing live backend

--- src/components/backendurl.js
+++ src/components/backendurl.js
@@ -1,1 +1,2 @@
-export const BACKEND_URL='http://localhost:3008/';
+// export const BACKEND_URL='http://localhost:3008/';/////////////////local
+export const BACKEND_URL='https://s1.zimlancer.co.zw/';/////////////////live
