a3f6df2a90511ff40c25ec7deaf7c521711fdd82
micpana <michaelmudimbu@gmail.com>
2019-06-21 09:57:02 +0200

    added cors settings for live frontend

--- index.js
+++ index.js
@@ -13,11 +13,16 @@ const bodyParser = require('body-parser')
 const fileUpload = require('express-fileupload')
 
 
-//cors stuff
+// //cors stuff for local frontend
+// var corsOptions = {
+//     origin: ['http://localhost:3000', 'http://localhost:3001' ]
+//     // optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+//   }
+  //cors stuff for live frontend
 var corsOptions = {
-    origin: ['http://localhost:3000', 'http://localhost:3001' ]
-    // optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
+  origin: ['https://zimlancer.co.zw' ]
+  // optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
 
 const app = express();
