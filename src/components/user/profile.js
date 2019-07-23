78ff2f7a17bbf420222fb0b37131cfa82b8d5d76
micpana <michaelmudimbu@gmail.com>
2019-07-24 01:44:47 +0200

    restoring this.props.match.params

--- src/components/user/profile.js
+++ src/components/user/profile.js
@@ -38,7 +38,6 @@ getSellerRating: []
     }
 
     componentDidMount() {
-      var userID= this.props.match.params.userid;
       const { cookies } = this.props;
       var today = new Date();
       var date = today.getFullYear()+'/'+(today.getMonth()+1)+'/'+today.getDate();
@@ -46,7 +45,7 @@ getSellerRating: []
       var dateTime = date+' '+time;
       axios.post(GRAPHQL_BASE_URL, {//////add profile view
         query: print(ADD_PROFILE_VIEW), variables: {
-          userid: userID,
+          userid: this.props.match.params.userid,
           viewerid: cookies.get('userId'),
           date: dateTime
         }
@@ -57,7 +56,7 @@ getSellerRating: []
       console.log(error.response)
   });//////ends here
       axios.post(GRAPHQL_BASE_URL, {/////////////get user details
-          query: print(GET_USER), variables: {id: userID}
+          query: print(GET_USER), variables: {id: this.props.match.params.userid}
       }).then((result) => {
           this.setState({userDetails: result.data.data.getUser});
 
@@ -65,7 +64,7 @@ getSellerRating: []
         console.log(error.response)
     });////////ends here
     axios.post(GRAPHQL_BASE_URL, {///////get all services by the user
-        query: print(GET_SERVICES_BY_USERID), variables: {id: userID}
+        query: print(GET_SERVICES_BY_USERID), variables: {id: this.props.match.params.userid}
     }).then((result) => {
         this.setState({userServices: result.data.data.getServicesByUserId});
 
@@ -73,7 +72,7 @@ getSellerRating: []
       console.log(error.response)
   });///////////////////ends here
   axios.post(GRAPHQL_BASE_URL, {/////////////////get orders in queue
-    query: print(GET_ORDERS_IN_QUEUE), variables: {sellerid: userID, completed: "false"}
+    query: print(GET_ORDERS_IN_QUEUE), variables: {sellerid: this.props.match.params.userid, completed: "false"}
 }).then((result) => {
     this.setState({ordersInQueue: result.data.data.getOrdersInQueue});
 
@@ -81,7 +80,7 @@ getSellerRating: []
   console.log(error.response)
 });///////////ends here
 axios.post(GRAPHQL_BASE_URL, {/////////////////get seller rating
-  query: print(GET_SELLER_RATING), variables: {sellerid: userID}
+  query: print(GET_SELLER_RATING), variables: {sellerid: this.props.match.params.userid}
 }).then((result) => {
   this.setState({getSellerRating: result.data.data.getSellerRating});
 
